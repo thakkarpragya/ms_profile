936a736816ab3242da68f3c828b9048d8689b4d3
Nikhil Vallore <nvallore@salesforce.com>
2022-09-25 10:16:18 +0530

    url change

--- ms_profile/urls.py
+++ ms_profile/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('profileuser.urls'))   
+    path('profile/admin/', admin.site.urls),
+    path('profile/', include('profileuser.urls'))   
 ]
